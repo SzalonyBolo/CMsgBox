4b713ddc30a3332867442911130b84aa820a32f9
Szalony Bolo <vaksus@gmail.com>
2021-07-20 15:08:40 +0200

    Linux so library

---
+++ EnvChange.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+extern "C" int MakeSomeNoise() {
+  //set new shell environmental variable using putenv
+  const char Varname[]="TMP";
+  const char VarValue[]="Hello World";
+  
+  setenv(Varname, VarValue, true);
+
+  std::cout << VarValue;
+
+  std::ofstream outfile (Varname);
+  outfile << VarValue << std::endl;
+  outfile.close();
+
+  return 0;
+}
